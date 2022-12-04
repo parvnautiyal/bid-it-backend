98d20b33118f3bd24cf62f305d3b03f129fb4915
Parv Nautiyal <105883196+parvnautiyal@users.noreply.github.com>
2022-12-05 03:59:18 +0530

    Initial commit

---
+++ src/test/java/com/project/bidit/BidItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bidit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargetTrashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
